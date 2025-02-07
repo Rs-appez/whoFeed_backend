09409a990ee90026e905bf13b0d14483b9a3e745
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-07 22:54:02 +0100

    add french field

--- whoFeed/game/models.py
+++ whoFeed/game/models.py
@@ -4,11 +4,14 @@ from django.db import models
 class Champion(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
     name = models.CharField(max_length=100)
+    fr_name = models.CharField(max_length=100)
     key = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     sprite = models.CharField(max_length=100)
     lore = models.TextField()
+    fr_lore = models.TextField()
     tags = models.ManyToManyField("Tag")
+    par_types = models.ManyToManyField("ParType")
 
     def __str__(self):
         return self.name
@@ -16,6 +19,7 @@ class Champion(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
+    fr_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -23,6 +27,7 @@ class Tag(models.Model):
 
 class ParType(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
+    fr_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
