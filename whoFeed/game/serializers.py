05a9ddeca67f32f3c3a454519a964fd1add3e691
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-09 17:38:35 +0100

    add all object in serializer

--- whoFeed/game/serializers.py
+++ whoFeed/game/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from .models import Champion, Tag, ParType
 
 
-class ChampionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Champion
-        fields = "__all__"
-
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
@@ -18,4 +12,13 @@ class TagSerializer(serializers.ModelSerializer):
 class ParTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParType
+        fields = "__all__"
+
+
+class ChampionSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    par_type = ParTypeSerializer()
+
+    class Meta:
+        model = Champion
         fields = "__all__"
