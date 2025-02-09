2230987281a948c36e18b1f4693eb67609a5e4c0
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-09 17:57:19 +0100

    add get_champions endpoint

--- whoFeed/game/views.py
+++ whoFeed/game/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Champion, Tag, ParType
 from .serializers import ChampionSerializer, TagSerializer, ParTypeSerializer
@@ -7,6 +9,12 @@ from .serializers import ChampionSerializer, TagSerializer, ParTypeSerializer
 class ChampionViewSet(viewsets.ModelViewSet):
     queryset = Champion.objects.all()
     serializer_class = ChampionSerializer
+
+    @action(detail=False)
+    def get_champions(self, request):
+        queryset = Champion.objects.all().order_by("?")[:30]
+        serializer = ChampionSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class TagViewSet(viewsets.ModelViewSet):
