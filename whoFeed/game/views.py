be54aaabe0b012d66eb37e5388d1f0ba23b8a5bd
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-13 21:30:24 +0100

    use jwt

--- whoFeed/game/views.py
+++ whoFeed/game/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from .jwt_utils import decode_jwt
 
 from .models import Champion, Tag, ParType, Player
 from .serializers import (
@@ -15,8 +18,19 @@ class ChampionViewSet(viewsets.ModelViewSet):
     queryset = Champion.objects.all()
     serializer_class = ChampionSerializer
 
-    @action(detail=False)
+    @action(detail=False, permission_classes=[AllowAny])
     def get_champions(self, request):
+        if "Authorization" not in request.headers:
+            return Response({"error": "No token provided"}, status=401)
+
+        token_type, token = request.headers.get("Authorization").split(" ")
+        if token_type != "Bearer":
+            return Response({"error": "Invalid token type"}, status=401)
+
+        player = Player.objects.filter(jwttoken=token).first()
+        if not player:
+            return Response({"error": "Invalid token"}, status=401)
+
         queryset = Champion.objects.all().order_by("?")[:50]
         serializer = ChampionSerializer(queryset, many=True)
         return Response(serializer.data)
